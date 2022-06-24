b5cf8732a9b5a5e64151101c7dd089e28f2be721
hariprasadh1 <hariprasadht2000@gmail.com>
2022-06-24 13:58:54 +0530

    Add project files.

---
+++ Controllers/claimController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using claimmicroservice.Models;
+using claimmicroservice.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace claimmicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class claimController : ControllerBase
+    {  
+        readonly log4net.ILog _log4net;
+        Imemberclaimrepo db;
+        public claimController(Imemberclaimrepo _db)
+        {
+            db = _db;
+            _log4net = log4net.LogManager.GetLogger(typeof(claimController));
+        }
+
+        /// <summary>
+        /// GET: api/<claimController>
+        /// </summary>
+        /// <returns>List<memberclaim></returns>
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _log4net.Info("claimController get called");
+            try
+            {
+               // memberclaimrepo ob = new memberclaimrepo();
+                return Ok(db.give());
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        /// <summary>
+        /// POST api/<claimController>
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>string</returns>
+
+        [HttpPost]
+        public IActionResult Post([FromBody] memberclaim obj)
+        {
+            _log4net.Info("claimController postmethod called");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                   // memberclaimrepo ob = new memberclaimrepo();
+                    db.create(obj);
+                    return Ok("SUCCESSFULLY ADDED");
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return NotFound();
+
+        }
+        /// <summary>
+        /// PUT api/<claimController>/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="obj"></param>
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] memberclaim obj)
+        {
+            _log4net.Info("claimController put called");
+            try
+            {
+                memberclaim obb = new memberclaim();
+                obb = db.GetClaimStatus(id, obj);
+                if (obb != null)
+                    return Ok(obb);
+                else
+                    return BadRequest(obb);
+            }
+            catch(Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Repository/memberclaimrepo.cs
@@ -0,0 +1,136 @@
+﻿using claimmicroservice.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace claimmicroservice.Repository
+{
+    public class memberclaimrepo : Imemberclaimrepo
+    {
+        Uri baseAddress;
+        HttpClient client;
+        IConfiguration _config;
+        public memberclaimrepo(IConfiguration config)
+        {
+            _config = config;
+            baseAddress = new Uri(_config["Links:POLICY"]);
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+
+        }
+        public static List<memberclaim> m = new List<memberclaim>()
+        {
+            new memberclaim()
+            {
+                memberid=1,
+                claimid=19172,
+                billedamount=1200,
+                claimedamount=1000,
+                claimstatus="PENDING",
+                benefitid=1
+            },
+            new memberclaim()
+            {
+                memberid=2,
+                claimid=18246,
+                billedamount=1200,
+                claimedamount=1000,
+                claimstatus="PENDING",
+                benefitid=1
+            },
+            new memberclaim()
+            {
+                memberid=3,
+                claimid=18940,
+                billedamount=1200,
+                claimedamount=1000,
+                claimstatus="PENDING",
+                benefitid=1
+            }
+         };
+
+        public void create(memberclaim ob)
+        {
+            Random rand = new Random();
+            ob.claimid = rand.Next(15000, 18000);
+            ob.claimstatus = "PENDING";
+            m.Add(ob);
+        }
+        //it is not used anymore
+        public List<memberclaim> fetchclaimsformember(int id)
+        {
+            List<memberclaim> l = new List<memberclaim>();
+            foreach (var item in m)
+            {
+                if (item.memberid == id)
+                {
+                    l.Add(item);
+                }
+            }
+            return l;
+        }
+        public List<memberclaim> give()
+        {
+
+            return m;
+        }
+      
+
+        public memberclaim GetClaimStatus(int id, memberclaim obj)
+        {
+            string s1 = obj.claimstatus;
+            List<int> ls = new List<int>();
+            int p = 0;
+            int op = 0;
+            HttpResponseMessage response1 = client.GetAsync(client.BaseAddress + "/policy/" + id).Result;
+            if (response1.IsSuccessStatusCode)
+            {
+                string data = response1.Content.ReadAsStringAsync().Result;
+                op = Convert.ToInt32(data);             
+                p = JsonConvert.DeserializeObject<int>(data);
+            }
+            int d = obj.benefitid;
+            HttpResponseMessage response2 = client.GetAsync(client.BaseAddress + "/policy/" + op + "/" + id + "/" + d).Result;
+            int o = 0;
+            if (response2.IsSuccessStatusCode)
+            {
+                string data = response2.Content.ReadAsStringAsync().Result;
+                o = Convert.ToInt32(data);               
+                                                       
+            }
+            if (obj.claimedamount > obj.billedamount)
+            {
+                //  return "Rejected";
+                obj.claimstatus = "REJECTED AS CLAIMED AMOUNT IS GREATER THAN BILLED AMOUNT";
+            }
+            else if (obj.claimedamount > o)
+            {
+                obj.claimstatus = "REJECTED AS THE CLAIMED AMOUNT EXCEEDS THE MAXIMUM AVAILABLE BENEFIT CHARGE";
+            }
+            else
+            {
+                obj.claimstatus = "ACCEPTED";
+            }
+            HttpResponseMessage response3 = client.GetAsync(client.BaseAddress + "/policy/" + op + "/" + id + "/" + d + "/1").Result;
+            int qo = 0;//it has topup
+            if (response3.IsSuccessStatusCode)
+            {
+                string data = response3.Content.ReadAsStringAsync().Result;
+                qo = Convert.ToInt32(data);
+            }
+            if (qo == 0)
+                obj.claimstatus = "REJECTED AS THERE IS NO TOPUP SUMMARY";
+            foreach (memberclaim item in m)
+            {
+                if (item.claimid == obj.claimid)
+                    item.claimstatus = obj.claimstatus;
+            }
+            return obj;
+
+        }
+    }
+}
